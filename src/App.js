c44c485c219b182804beb47c92ad4b14442d2657
Mohamed Hatem <mohamedhatemakl@gmail.com>
2021-07-13 01:19:12 +0200

    react router setup in our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 
 function App() {
-  return <h4>Furniture E-commerce App</h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route path="/checkout" exact>
+          <Checkout />
+        </Route>
+        <Route path="*" exact>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
